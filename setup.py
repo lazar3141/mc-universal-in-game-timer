7aeb68940705319ab9f2130f9f0112cd0d2905b6
Ashwin Innuganti <ashwininnuganti@Ashwins-MacBook-Pro.local>
2020-08-01 16:36:18 -0400

    It has come to my attention that py2exe sucks, so I removed the setup function for Windows

--- setup.py
+++ setup.py
@@ -1,46 +1,20 @@
-try:
-    from setuptools import setup
-except:
-    from distutils.core import setup
+
+#* This file uses py2app to make only a .app bundle for MacOS by running `python3 setup.py py2app`.
+#* Windows and Linux executables are made separately with PyInstaller (https://pyinstaller.readthedocs.io/en/stable/index.html).
+
+from setuptools import setup
+
 import os
-import sys
-
-APP = "main.py"
-DATA_FILES = ["VERSION.txt", "SettingsWindow.ui"]
 
 
-if sys.platform == "win32":
-    for f in os.listdir(os.path.dirname(__file__), "Resources"):
-        DATA_FILES.append(("Resources", [os.path.dirname(__file__), "Resources", f]))
-
-    setup(
-        name="Minecraft Universal In-Game Timer",
-        windows=[
-            {
-                "script": APP,
-                "icon_resources": [(1, os.path.join(os.path.dirname(__file__), "Resources", "icons.ico"))]
-            }
-        ],
-        data_files=DATA_FILES,
-        options={"py2exe": {
-            "packages": ["PyQt5", "nbt"],
-            "bundle_files": 1,
-            "compressed": True,
-        }},
-        zipfile=None,
-        setup_requires=["py2exe"],
-    )
-
-
-elif sys.platform == "darwin":
-    setup(
-        name="Minecraft Universal In-Game Timer",
-        app=[APP],
-        data_files=DATA_FILES,
-        options={"py2app": {
-            "packages": ["PyQt5", "nbt"],
-            "iconfile": os.path.join(os.path.dirname(__file__), "Resources", "icons.icns"),
-            "resources": "Resources",
-        }},
-        setup_requires=["py2app"],
-    )
+setup(
+    name="Minecraft Universal In-Game Timer",
+    app=["main.py"],
+    data_files=["VERSION.txt", "SettingsWindow.ui"],
+    options={"py2app": {
+        "packages": ["PyQt5", "nbt"],
+        "iconfile": os.path.join("Resources", "icons.icns"),
+        "resources": "Resources",
+    }},
+    setup_requires=["py2app"],
+)
